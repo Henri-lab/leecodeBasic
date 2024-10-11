45f44632863b399fd973a864638ee5802654c5fe
henriFox.W <62045411+Henri-lab@users.noreply.github.com>
2024-10-11 13:06:45 +0800

    ''

---
+++ src/test.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+        unordered_map<int, int> hash;
+
+
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (hash.find(complement) != hash.end()) {
+                result.push_back(hash[complement]);
+                result.push_back(i);
+                return result;
+            }
+            hash[nums[i]] = i;
+        }
+
+        return result;
+    }
+};
