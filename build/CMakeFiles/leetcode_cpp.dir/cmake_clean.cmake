682368a281a1ebcadcb702f0be9f15a10507777e
henriFox.W <62045411+Henri-lab@users.noreply.github.com>
2024-10-11 10:56:43 +0800

    ‘’

---
+++ build/CMakeFiles/leetcode_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/leetcode_cpp.dir/main.cpp.o"
+  "CMakeFiles/leetcode_cpp.dir/main.cpp.o.d"
+  "leetcode_cpp"
+  "leetcode_cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/leetcode_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
